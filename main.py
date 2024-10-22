01b52f7a80cc17fa4bf7e237c02d74490215613a
VilladsHJ <Villads.joergensen@gmail.com>
2024-10-22 21:47:24 +0200

    added asteroids that spawn randomly and a player who can move their spaceship

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -7,12 +10,51 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock_obj = pygame.time.Clock()
+    
+    fr = 60
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in asteroids:
+            if obj.collision(player):
+                print("Game over!")
+                return False
+
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        pygame.display.flip()
+        
+        #restrict frame rate
+        dt = clock_obj.tick(fr)/1000
+        
+
+       
+        
+
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,56 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self,x,y):
+        super().__init__(x,y,PLAYER_RADIUS)
+        self.rotation = 0
+    
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        surface = screen
+        color = "white"
+        points = self.triangle()
+        width = 2
+        return pygame.draw.polygon(surface, color, points, width)
+    
+    def rotate(self, dt):
+        self.rotation += dt*PLAYER_TURN_RADIUS
+        return self.rotation
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LSHIFT]:
+            dt = self.speed_boost(dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)          
+        if keys[pygame.K_q]:
+            self.strafe(dt)
+        if keys[pygame.K_e]:
+            self.strafe(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
+    def strafe(self, dt):
+        strafe_right = pygame.Vector2(1, 0).rotate(self.rotation)
+        self.position += strafe_right * PLAYER_SPEED * dt
+    
+    def speed_boost(self, dt):
+        return dt*2
